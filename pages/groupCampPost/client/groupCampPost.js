cecc26db0f4b31094c31293284c7c60ed45c2d02
Abhishek Kulkarni <abhishek@brandeis.edu>
2016-07-15 11:23:26 -0400

    upgraded tags in Group Camp

--- pages/groupCampPost/client/groupCampPost.js
+++ pages/groupCampPost/client/groupCampPost.js
@@ -9,15 +9,17 @@ Template.groupCampPost.events({
 	"click .js-submit": function(event){
 		console.log("Submitted!");
 
+		tags = $(".js-tags").val().split(",");
+		for (i = 0; i < tags.length; i++) 	{tags[i] = tags[i].trim();}
+
 		GroupCampTrips.insert({
 			author: $(".js-author").val(),
 			timestamp: new Date(),
 
 			title: $(".js-title").val(),
-
 			message: $(".js-message").val(),
-
 			description: $(".js-description").val(),
+			tags: tags,
 
 			destination: $(".js-destination").val(), //.toLowerCase(),
 			from: $(".js-from").val(),
