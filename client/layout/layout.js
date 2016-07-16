b833aa0dd177c315367d91e07d703435dc4ee08c
Jackthesuper <jack.shenzijie@outlook.com>
2016-07-16 00:08:18 -0400

    name dropdown

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,7 +1,16 @@
+Template.layout.helpers({
+  userName: function() {
+      const liveUser = Meteor.userId();
+      return Meteor.users.findOne({
+          _id: liveUser
+      });
+  },
+})
 Template.layout.events({
     'click .logout': function(event){
         event.preventDefault();
         Meteor.logout();
         Router.go('/');
-    }
+    },
+
 });
