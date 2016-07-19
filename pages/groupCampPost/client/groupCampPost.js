dcf3092ac2bea27ea6c814ed312b93ebcd84d25e
Abhishek Kulkarni <abhishek@brandeis.edu>
2016-07-19 14:46:43 -0400

    forces users to fill in required input boxes and added validation state css

--- pages/groupCampPost/client/groupCampPost.js
+++ pages/groupCampPost/client/groupCampPost.js
@@ -10,31 +10,138 @@ Template.groupCampPost.events({
 	"click .js-submit": function(event){
 		event.preventDefault();
 
-		tags = $(".js-tags").val().split(",");
-		for (i = 0; i < tags.length; i++) 	{tags[i] = tags[i].trim();}
-		tags = tags.sort();
+		author = Meteor.user().userName;
+		timestamp = new Date();
 
-		GroupCampTrips.insert({
-			author: Meteor.user().userName,
-			timestamp: new Date(),
+		title = $(".js-title").val();
+		message = $(".js-message").val();
+		description = $(".js-description").val();
+		rawTags = $(".js-tags").val();
+		tags = [];
 
-			title: $(".js-title").val(),
-			message: $(".js-message").val(),
-			description: $(".js-description").val(),
-			tags: tags,
+		destination = $(".js-destination").val();
+		From = $(".js-from").val();
+		to = $(".js-to").val();
 
-			destination: $(".js-destination").val(),
-			from: $(".js-from").val(),
-			to: $(".js-to").val(),
+		travelers = [];
+		threshold = $(".js-threshold").val();
 
-			travelers: [],
-			threshold: $(".js-threshold").val(),
+		cost = $(".js-cost").val();
+		link = $(".js-link").val();
+		picture = $(".js-picture").val();
 
-			cost: $(".js-cost").val(),
-			link: $(".js-link").val(),
-			picture: $(".js-picture").val()
-		});
 
-		Router.go("groupCampSearch");
+		complete = true;
+
+
+		// TITLE
+		if (!title){	// Required
+			complete = false;
+			$(".js-titleGroup").removeClass('has-success').addClass('has-error');
+		}
+		else {
+			$(".js-titleGroup").removeClass('has-error').addClass('has-success');
+		}
+
+		// MESSAGE
+		if (!message){
+			complete = false;
+			$(".js-messageGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			$(".js-titleGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+		// DESCRIPTION
+		if (!description){
+			complete = false;
+			$(".js-descriptionGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			$(".js-descriptionGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+		// TAGS
+		if (!rawTags){
+			complete = false;
+			$(".js-tagsGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			tags = $(".js-tags").val().split(",");
+			for (i = 0; i < tags.length; i++) 	{tags[i] = tags[i].trim();}
+			tags = tags.sort();
+			$(".js-tagsGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+		// DESTINATION
+		if (!destination){	// Required
+			complete = false;
+			$(".js-destinationGroup").removeClass('has-success').addClass('has-error');
+		}
+		else {
+			$(".js-destinationGroup").removeClass('has-error').addClass('has-success');
+		}
+
+		// FROM
+		if (!From){	// Required
+			complete = false;
+			$(".js-fromGroup").removeClass('has-success').addClass('has-error');
+		}
+		else {
+			$(".js-fromGroup").removeClass('has-error').addClass('has-success');
+		}
+
+		// TO
+		if (!to){	// Required
+			complete = false;
+			$(".js-toGroup").removeClass('has-success').addClass('has-error');
+		}
+		else {
+			$(".js-toGroup").removeClass('has-error').addClass('has-success');
+		}
+
+		// THRESHOLD
+		if (!threshold){	// Required
+			complete = false;
+			$(".js-thresholdGroup").removeClass('has-success').addClass('has-error');
+		}
+		else {
+			$(".js-thresholdGroup").removeClass('has-error').addClass('has-success');
+		}
+
+		// COST
+		if (!cost){
+			complete = false;
+			$(".js-costGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			$(".js-costGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+		// LINK
+		if (!link){
+			complete = false;
+			$(".js-linkGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			$(".js-linkGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+		// PICTURE
+		if (!picture){
+			complete = false;
+			$(".js-pictureGroup").removeClass('has-success').addClass('has-warning');
+		}
+		else {
+			$(".js-pictureGroup").removeClass('has-warning').addClass('has-success');
+		}
+
+
+
+		if (complete){
+			GroupCampTrips.insert({author, timestamp, title, message, description, tags: tags, destination, from: From, to, travelers, threshold, cost, link, picture});
+			Router.go("groupCampSearch");
+		}
+		else{}
 	}
 })
