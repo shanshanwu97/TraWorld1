2c6b2402dff87220445df3e721213a76848096b7
zimucui <1015105473@qq.com>
2016-07-21 14:29:51 -0400

    finished map

--- pages/googlemapapi/client/googlemapapi.js
+++ pages/googlemapapi/client/googlemapapi.js
@@ -1,11 +1,14 @@
 Template.map.onCreated(function () {
   Session.set("location",{lat:42,lng:-71})
-  GoogleMaps.load();
+  GoogleMaps.load({ v: '3', key: 'AIzaSyB7-F_RespGrP0zUzQO4AglkouFbTeKp0c', libraries: '' });
   GoogleMaps.ready('naviMap',function(map) {
     var markerCurrent = new google.maps.Marker({
         position: new google.maps.LatLng(Session.get("location").lat,Session.get("location").lng),
         map:map.instance
       });
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(GoogleMaps.maps.naviMap.instance);
     Tracker.autorun(function() {
       map.instance.setCenter(new google.maps.LatLng(Session.get("location").lat,Session.get("location").lng))
       
@@ -51,50 +54,17 @@ Template.map.helpers({
 });
 
 Template.map.events({
-  "click .js-addDay": function(event){
-    console.log("hey you clicked the button");
-
-    const location =$(".js-location").val();
-
-    console.log(location);
-
-
-    $.ajax({
-
-      url:"https://maps.googleapis.com/maps/api/geocode/json",
-
-        data:{
-
-          "address":location,
-
-          "key": "AIzaSyB7-F_RespGrP0zUzQO4AglkouFbTeKp0c",
-
-        },
-
-        dataType:"json"
-
-    }).done(function( data ) {
-
-    if ( console && console.log ) {
-
-      Session.set( "location",data.results[0].geometry.location );
-
-
-    }
-    $(".js-location").val("");
-    $(".js-talk").html("Try a new place!");
-
-
-  })
+  
 
 
 
-  },
+  
 
 
   "click .js-talk": function(event){
       console.log("clicked it");
       $(".js-talk").html("Listening...");
+      event.preventDefault();
    // https://shapeshed.com/html5-speech-recognition-api/
       const recognition = new webkitSpeechRecognition();
       recognition.lang = 'en-US' 
@@ -102,17 +72,48 @@ Template.map.events({
           console.dir(event);
           $(".js-talk").html("Got it!");
           Session.set("transcript",event.results[0][0].transcript);
-          $(".js-location").val(Session.get("transcript"));
+          $("#from").val(Session.get("transcript"));
  
           
 //        execute(Session.get("transcript")); 
         };
-        $(".js-location").val("");
+        $("#from").val("");
     recognition.start();
    //      console.log("starting the recognizer")
 
     
-   },   
+   },  
+
+ "click .js-talk2": function(event){
+      console.log("clicked it");
+      $(".js-talk2").html("Listening...");
+      event.preventDefault();
+   // https://shapeshed.com/html5-speech-recognition-api/
+      const recognition = new webkitSpeechRecognition();
+      recognition.lang = 'en-US' 
+      recognition.onresult = function(event) {
+          console.dir(event);
+          $(".js-talk2").html("Got it!");
+          Session.set("transcript",event.results[0][0].transcript);
+          $("#to").val(Session.get("transcript"));
+ 
+          
+//        execute(Session.get("transcript")); 
+        };
+        $("#to").val("");
+    recognition.start();
+   //      console.log("starting the recognizer")
+
+    
+   },  
+   "click #clickme": function(event){
+    const from = $("#from").val();
+    const to = $("#to").val();
+    event.preventDefault();
+    console.log("getting directions "+from+" "+to);
+    calculateRoute(from, to);
+
+   }
 })
 
 function send() {
@@ -159,6 +160,73 @@ function setResponse(val) {
   $("#response").text(val);
 }
 
+
+function calculateRoute(from, to) {
+        //var directionsService = new google.maps.DirectionsService();
+        var directionsRequest = {
+          origin: from,
+          destination: to,
+          travelMode: google.maps.DirectionsTravelMode.DRIVING,
+          unitSystem: google.maps.UnitSystem.METRIC
+        };
+        console.log("sending request");console.dir(directionsRequest);
+        directionsService.route(
+          directionsRequest,
+          function(response, status)
+          { console.dir([status,response, new Date()]);
+            if (status == google.maps.DirectionsStatus.OK)
+            {
+              console.log("routing");
+              //new google.maps.DirectionsRenderer({
+               // map: GoogleMaps.maps.naviMap.instance,
+                //directions: response
+              //});
+
+              directionsDisplay.setDirections(response);
+            }
+            else
+              $("#error").append("Unable to retrieve your route<br />");
+          }
+        );
+      }
+
+      $(document).ready(function() {
+        // If the browser supports the Geolocation API
+        if (typeof navigator.geolocation == "undefined") {
+          $("#error").text("Your browser doesn't support the Geolocation API");
+          return;
+        }
+
+        $("#from-link, #to-link").click(function(event) {
+          event.preventDefault();
+          var addressId = this.id.substring(0, this.id.indexOf("-"));
+
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({
+              "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+            },
+            function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK)
+                $("#" + addressId).val(results[0].formatted_address);
+              else
+                $("#error").append("Unable to retrieve your address<br />");
+            });
+          },
+          function(positionError){
+            $("#error").append("Error: " + positionError.message + "<br />");
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 10 * 1000 // 10 seconds
+          });
+        });
+
+        $("#calculate-route").submit(function(event) {
+          event.preventDefault();
+          calculateRoute($("#from").val(), $("#to").val());
+        });
+      });
 
 
 
