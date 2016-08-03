06270399ca0c9bcd1821eaef24bbad1ebde0c71d
Abhishek Kulkarni <abhishek@brandeis.edu>
2016-08-03 13:09:46 -0400

    specific error messages for error fields

--- pages/groupCampPost/client/groupCampPost.js
+++ pages/groupCampPost/client/groupCampPost.js
@@ -14,6 +14,12 @@ Template.groupCampPost.rendered = function(){
 Template.groupCampPost.helpers({
 	setUp: function(){
 		Session.set("notCompleteAlertNeeded", false);
+		Session.set("ErrorMessage-title", null);
+		Session.set("ErrorMessage-destination", null);
+		Session.set("ErrorMessage-from", null);
+		Session.set("ErrorMessage-to", null);
+		Session.set("ErrorMessage-deadline", null);
+		Session.set("ErrorMessage-threshold", null);
 	},
 
 	trips:function(){
@@ -27,6 +33,10 @@ Template.groupCampPost.helpers({
 			return true;
 		else
 			return false;
+	},
+
+	getError: function(type) {
+		return Session.get("ErrorMessage-" + type);
 	}
 })
 
@@ -66,9 +76,11 @@ Template.groupCampPost.events({
 			complete = false;
 			filled = false;
 			$(".js-titleGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-title", "Your trip must have a name!");
 		}
 		else {
 			$(".js-titleGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-title", null);
 		}
 
 		// MESSAGE
@@ -106,49 +118,95 @@ Template.groupCampPost.events({
 			complete = false;
 			filled = false;
 			$(".js-destinationGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-destination", "Your trip must have a destination!");
 		}
 		else {
 			$(".js-destinationGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-destination", null);
 		}
 
 		// FROM
-		if (!From || isNaN(Date.parse(From)) || new Date(From) < new Date()){	// Required
+		if (!From || isNaN(Date.parse(From))){	// Required
 			complete = false;
 			filled = false;
 			$(".js-fromGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-from", "Your trip must have a departure date!");
+		}
+		else if (new Date(From) < new Date()) {
+			complete = false;
+			filled = false;
+			$(".js-fromGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-from", "You can't plan a trip in the past!");
 		}
 		else {
 			$(".js-fromGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-from", null);
 		}
 
 		// TO
-		if (!to || isNaN(Date.parse(to)) || new Date(to) < new Date(From) || new Date(to) < new Date()){	// Required
+		if (!to || isNaN(Date.parse(to))){	// Required
 			complete = false;
 			filled = false;
 			$(".js-toGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-to", "Your trip must have an arival date!");
+		}
+		else if (new Date(to) < new Date(From)) {
+			complete = false;
+			filled = false;
+			$(".js-toGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-to", "You can't came back before you leave!");
+		}
+		else if (new Date(to) < new Date()) {
+			complete = false;
+			filled = false;
+			$(".js-toGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-to", "You can't plan a trip in the past!");
 		}
 		else {
 			$(".js-toGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-to", null);
 		}
 
 		//DEADLINE
-		if (!deadline || isNaN(Date.parse(deadline)) || new Date(deadline) > new Date(From) || new Date(deadline) < new Date()){	// Required
+		if (!deadline || isNaN(Date.parse(deadline))){	// Required
 			complete = false;
 			filled = false;
 			$(".js-deadlineGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-deadline", "Your trip must have a deadline!");
+		}
+		else if (new Date(deadline) > new Date(From)) {
+			complete = false;
+			filled = false;
+			$(".js-deadlineGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-deadline", "Your deadline must be before your departure date!");
+		}
+		else if (new Date(deadline) < new Date()) {
+			complete = false;
+			filled = false;
+			$(".js-deadlineGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-deadline", "You need to give other travelers a chance to sign up!");
 		}
 		else {
 			$(".js-deadlineGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-deadline", null);
 		}
 
 		// THRESHOLD
-		if (!threshold || threshold < 1 || Math.floor(threshold) != threshold){	// Required
+		if (!threshold || threshold < 2){	// Required
 			complete = false;
 			filled = false;
 			$(".js-thresholdGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-threshold", "You must require at least one other person to go with you!");
+		}
+		else if (Math.floor(threshold) != threshold) {
+			complete = false;
+			filled = false;
+			$(".js-thresholdGroup").removeClass('has-success').addClass('has-error');
+			Session.set("ErrorMessage-threshold", "Travelers are discrete units. Please select a whole number.");
 		}
 		else {
 			$(".js-thresholdGroup").removeClass('has-error').addClass('has-success');
+			Session.set("ErrorMessage-threshold", null);
 		}
 
 		// COST
